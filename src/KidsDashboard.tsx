c46a85418a05caf517bc8688ee911788816ddaa7
kerialynn91-beep <kerialynn91@gmail.com>
2026-06-29 11:24:29 -0500

    Fix habitat key from OCEAN to BEACH

--- src/KidsDashboard.tsx
+++ src/KidsDashboard.tsx
@@ -56,7 +56,7 @@ const getHabitatForAvatar = (avatar: string): HabitatType => {
     [HABITATS.POND]: [
       '🐸', '🐢', '🦆', '🦩'
     ],
-    [HABITATS.OCEAN]: [
+    [HABITATS.BEACH]: [
       '🦀', '🐚', '🦞', '🦭', '🐧', '🐬',
     ],
   };
